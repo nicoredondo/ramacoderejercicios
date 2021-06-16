24f1fd47b6ae16f3556c8e221246c0a4b64abfc1
nicoredondo <59586290+nicoredondo@users.noreply.github.com>
2021-06-16 16:56:43 -0300

    Add files via upload

--- Pa probar/pruebas.js
+++ Pa probar/pruebas.js
@@ -1,14 +1,24 @@
-var nombreApellido = prompt('Ingresa tu nombre y apellido')
-alert('Hola ' + nombreApellido + '. Vamos a calcular cuantos días de vida tenes')
-var edad = prompt('Por favor ingresa tu edad')
-parseInt(edad)
-var dias = edad * 365
-alert('En total tenes ' + dias + ' dias de vida')
-if (edad > 17) {
-    alert('Sos mayor de edad, podes seguir navegando')
+var nombreApellido = prompt('Ingresa tu nombre y apellido por favor.');
+alert(`Hola ${nombreApellido}. Vamos a calcular cuantos días de vida tenes.`);
+var edad = parseInt(prompt('Por favor ingresa tu edad.'));
+var dias = edad * 365;
+var horas = dias * 24;
+var minutos = horas * 60;
+var segundos = minutos * 60;
+alert(`En total tenes ${dias} dias de vida`);
+var bMayor = edad > 17;
+if (bMayor) {
+    alert('Sos mayor de edad, podes seguir navegando');
 } else {
-    alert('Sos menor de edad, no podes seguir navegando')
-}
+    alert('Sos menor de edad, no podes seguir navegando');
+};
+while (bMayor) {
+    alert(`En total tenes ${horas} horas de vida`);
+    alert(`En total tenes ${minutos} minutos de vida`);
+    alert(`En total tenes ${segundos} segundos de vida`);
+    alert(`Que viejo que sos ${nombreApellido}. Yo que vos me empiezo a cuidar!`)
+    break;
+};
 
 //String interpolation
 //alert(`En total tenes ${dias} dias de vida`)//
